e262b7621f527a4f0c8a411c0c21506d1ce52ee3
d7p4n4 <bercikezs199999999@gmail.com>
2019-10-20 19:14:55 +0100

    1.20191020.6

--- Generator(.net framework)/Program.cs
+++ Generator(.net framework)/Program.cs
@@ -40,7 +40,7 @@ namespace Generator_.net_framework_
         static void Main(string[] args)
             {
 
-            //Date: 2019. 10. 20. 14:33 
+            //Date: 2019. 10. 20. 19:15
 
             log.Debug("path:"+ GetAppConfigStringParameter(APPSETTINGS_CLASSNAME));
 
